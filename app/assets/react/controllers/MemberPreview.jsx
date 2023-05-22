afbd22bd35d974ebc4cbc56827fb99e94287cdca
Timboo352 <timboo352@gmail.com>
2023-05-23 00:19:21 +0200

    New member button

--- app/assets/react/controllers/MemberPreview.jsx
+++ app/assets/react/controllers/MemberPreview.jsx
@@ -4,7 +4,7 @@ export default function (props) {
     return(
         <a href={props.path} className="member-preview__link">
             <div className="member-preview">
-                <h2 className="member-preview__title">{props.lastName} {props.firstName} | {props.nickname}</h2>
+                <h2 className="member-preview__title">{props.lastName} {props.firstName} <small>| {props.nickname}</small></h2>
                 <div className="member-preview__role__container">
                     <div className="member-preview__role__background" style={{background: "linear-gradient(90deg, rgba(0,0,0,0) 0%, #" + props.roleColor + " 100%)"}}></div>
                     <span className="member-preview__role__title" style={{color: "#" + props.roleColor}}>
